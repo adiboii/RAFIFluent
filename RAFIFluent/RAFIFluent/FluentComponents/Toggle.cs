58f7db48f25f3a3a04e740cdcb1bfd90f64f8e1f
adi.boyii <adrianpauln.reyes@gmail.com>
2022-07-12 14:14:31 +0800

    changed toggle xaml to class

---
+++ RAFIFluent/RAFIFluent/FluentComponents/Toggle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+namespace RAFIFluent.FluentComponents
+{
+    public class Toggle : Switch
+    {
+        FluentColor colors = new FluentColor();
+
+        public Toggle()
+        {
+            InitVisualStates();
+        }
+
+        void InitVisualStates()
+        {
+            VisualStateManager.SetVisualStateGroups(this, new VisualStateGroupList
+            {
+                new VisualStateGroup
+                {
+                    Name="CommonStates",
+                    TargetType=typeof(Switch),
+                    States =
+                    {
+                        new VisualState
+                        {
+                            Name = "On",
+                            TargetType = typeof(Switch),
+                            Setters =
+                            {
+                               new Setter { Property = OnColorProperty, Value = colors.ThemeTertiary },
+                               new Setter { Property = ThumbColorProperty, Value = colors.ThemePrimary }
+                            }
+                        },
+                        new VisualState
+                        {
+                            Name = "Off",
+                            TargetType = typeof(Switch),
+                            Setters =
+                            {
+                               new Setter { Property = ThumbColorProperty, Value = colors.NeutralLight }
+                            }
+
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+ 
+}
