e745b19f9db5e40cbf72117aa803ea3e4599226e
adi.boyii <adrianpauln.reyes@gmail.com>
2022-07-12 14:09:22 +0800

    changed checkbox xaml to class

---
+++ RAFIFluent/RAFIFluent/FluentComponents/CheckBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RAFIFluent.FluentComponents
+{
+    public class CheckBox : Xamarin.Forms.CheckBox
+    {
+
+        FluentColor colors = new FluentColor();
+
+        public CheckBox()
+        {
+            InitVisualStates();
+        }
+
+        void InitVisualStates()
+        {
+            VisualStateManager.SetVisualStateGroups(this, new VisualStateGroupList
+            {
+                new VisualStateGroup
+                {
+                    Name="CommonStates",
+                    TargetType=typeof(CheckBox),
+                    States =
+                    {
+                        new VisualState
+                        {
+                            Name = "Normal",
+                            TargetType = typeof(Xamarin.Forms.CheckBox),
+                            Setters =
+                            {
+                                new Setter {Property = ColorProperty, Value = colors.NeutralTertiaryAlt }
+                            }
+                        },
+                        new VisualState
+                        {
+                            Name = "IsChecked",
+                            TargetType = typeof(Xamarin.Forms.CheckBox),
+                            Setters =
+                            {
+                                new Setter { Property = ColorProperty , Value = colors.ThemePrimary }
+                            }
+
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
+
