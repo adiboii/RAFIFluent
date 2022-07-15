6a150015e505f0203ae81447b1f3b2c9a186b408
adi.boyii <adrianpauln.reyes@gmail.com>
2022-07-15 08:51:02 +0800

    changed class names of textbox

--- RAFIFluent/RAFIFluent/FluentComponents/FramedTextBox.cs
+++ RAFIFluent/RAFIFluent/FluentComponents/FramedTextBox.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FFImageLoading.Svg.Forms;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
@@ -6,59 +7,73 @@ using Xamarin.Forms;
 
 namespace RAFIFluent.FluentComponents
 {
-    public class FluentTextBox : Xamarin.Forms.Frame
+    public class FramedTextBox : Xamarin.Forms.Frame
     {
         FluentColor colors = new FluentColor();
         TextBox textbox = new TextBox();
+        Icon icon = new Icon();
 
         public static readonly BindableProperty placeHolder = BindableProperty.Create(
-           "PlaceHolder", typeof(string), typeof(FluentTextBox), "Search");
+           "PlaceHolder", typeof(string), typeof(FramedTextBox), "Search");
         public string PlaceHolder 
         { 
-            get { return (string)GetValue(FluentTextBox.placeHolder); }
+            get { return (string)GetValue(FramedTextBox.placeHolder); }
             set 
             { 
-                SetValue(FluentTextBox.placeHolder, value);
+                SetValue(FramedTextBox.placeHolder, value);
                 textbox.Placeholder = value;
             } 
         }
 
         public static readonly BindableProperty hasIcon = BindableProperty.Create(
-            "HasIcon", typeof(bool), typeof(FluentTextBox), false);
+            "HasIcon", typeof(bool), typeof(FramedTextBox), false);
 
         public bool HasIcon 
         { 
-            get { return (bool)GetValue(FluentTextBox.hasIcon); } 
-            set { SetValue(FluentTextBox.hasIcon, value); }
+            get { return (bool)GetValue(FramedTextBox.hasIcon); } 
+            set { SetValue(FramedTextBox.hasIcon, value); }
         }
 
         public static readonly BindableProperty textColor = BindableProperty.Create(
-         "TextColor", typeof(Color), typeof(FluentTextBox), Color.Black);
+         "TextColor", typeof(Color), typeof(FramedTextBox), Color.Black);
 
         public Color TextColor
         {
-            get { return (Color)GetValue(FluentTextBox.textColor); }    
+            get { return (Color)GetValue(FramedTextBox.textColor); }    
             set 
             { 
-                SetValue(FluentTextBox.textColor, value);
+                SetValue(FramedTextBox.textColor, value);
                 textbox.TextColor = value;
             }
         }
 
         public static readonly BindableProperty placeholderColor = BindableProperty.Create(
-        "PlaceholderColor", typeof(Color), typeof(FluentTextBox), Color.Black);
+        "PlaceholderColor", typeof(Color), typeof(FramedTextBox), Color.Black);
 
         public Color PlaceholderColor
         {
-            get { return (Color)GetValue(FluentTextBox.placeholderColor); }
+            get { return (Color)GetValue(FramedTextBox.placeholderColor); }
             set 
             { 
-                SetValue(FluentTextBox.placeholderColor, value);
+                SetValue(FramedTextBox.placeholderColor, value);
                 textbox.PlaceholderColor = value;
             }
         }
 
-        public FluentTextBox()
+        public static readonly BindableProperty iconSource = BindableProperty.Create(
+          "IconSource", typeof(string), typeof(Icon), "Logo.svg");
+
+        public string IconSource
+        {
+            get { return (string)GetValue(Icon.iconSource); }
+            set 
+            { 
+                SetValue(Icon.iconSource, value); 
+                icon.Source = value;
+            }
+        }
+
+        public FramedTextBox()
         {
             BackgroundColor = colors.NeutralLight;
             Padding = new Thickness(10, 0);
@@ -71,11 +86,10 @@ namespace RAFIFluent.FluentComponents
             textbox.HorizontalOptions = LayoutOptions.FillAndExpand;
             textbox.ClearButtonVisibility = ClearButtonVisibility.WhileEditing;
             //image to add later
-            Label l = new Label();  
-            l.Text = "O";
-            l.VerticalOptions = LayoutOptions.Center;
-            l.HorizontalOptions = LayoutOptions.Center;
-            stack.Children.Add(l);
+            icon.IconSource = IconSource;
+            icon.HeightRequest = 20;
+            icon.WidthRequest = 20;
+            stack.Children.Add(icon);
             stack.Children.Add(textbox);
             Content = stack;
         }
